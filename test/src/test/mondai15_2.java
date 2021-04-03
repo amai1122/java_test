065b224cfec211538f69174930fbb4ef7db15cba
P056_amai <P056_amai@LAPTOP-7O5H7RT7>
2021-04-03 11:12:19 +0900

    問題15

--- test/src/test/mondai15_2.java
+++ test/src/test/mondai15_2.java
@@ -4,6 +4,7 @@ public class mondai15_2 extends mondai15_1 {
 
 	public int[] multiplyResult() {
 		int[] arr = super.multiplyResult();
+
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = arr[i] * 2;
 		}
