2b46eca254221c0fd5025e2c31ca843ffcfa144e
P056_amai <P056_amai@LAPTOP-7O5H7RT7>
2021-04-04 23:35:48 +0900

    単元テスト15の内容

--- test/src/test/mondai15.java
+++ test/src/test/mondai15.java
@@ -3,7 +3,15 @@ package test;
 public class mondai15 {
 
 	public static void main(String[] args) {
-		
+
+		mondai15_2 mo2 = new mondai15_2();
+
+		int[] a = mo2.multiplyResult();
+
+		for (int set = 0; set < a.length; set++) {
+			System.out.println(a[set]);
+		}
+
 	}
 
 }
--- test/src/test/mondai15_1.java
+++ test/src/test/mondai15_1.java
@@ -1,10 +1,14 @@
 package test;
 
 public class mondai15_1 {
-//	public int[] multiplyResult() {
+	public int[] multiplyResult() {
+		int[] a_1 = new int[9];
 
+		for (int i = 1; i <= 9; i++) {
 
-//		return int a=1;
-//	}
+			a_1[i - 1] = 1 * i;
+		}
+		return a_1;
+	}
 
 }
