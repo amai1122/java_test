b8e82c462b6583838d4923423240728b998e77b4
P056_amai <P056_amai@LAPTOP-7O5H7RT7>
2021-04-06 00:56:52 +0900

    2021/4/6

---
+++ test/mondai16/test/Foo.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Foo {
+	public int calculate(int a, double b) {
+		int i = (int) (a + b);
+		return i;
+
+	}
+	public int calculate(int a, int b) {
+		int i = (int) (a - b);
+		return i;
+
+	}
+
+
+}
